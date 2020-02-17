ba97a65af17168a6edff8ce519c2e2dad689c58b
KEVINRO26 <56985515+KEVINRO26@users.noreply.github.com>
2020-02-16 21:41:09 -0500

    Add files via upload

---
+++ CribaTest.java
@@ -0,0 +1,36 @@
+
+package procesossoftware;
+import junit.framework.*;
+
+
+public class CribaTest extends TestCase{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        junit.swingui.TestRunner.main(new String[] {"CribaTest"});
+    }
+    
+    
+    public CribaTest(String nombre){
+        super(nombre);
+    }
+    public void testPrimos(){
+        int[] nullArray = Criba.generarPrimos(0);
+        assertEquals(nullArray.length, 0);
+        int[] minArray = Criba.generarPrimos(2);
+        assertEquals(minArray.length, 1);      
+        assertEquals(minArray[0], 2);
+        
+        int[] threeArray = Criba.generarPrimos(3);      
+        assertEquals(threeArray.length, 2);      
+        assertEquals(threeArray[0], 2);      
+        assertEquals(threeArray[1], 3);
+        
+        int[] centArray = Criba.generarPrimos(100);      
+        assertEquals(centArray.length, 25);      
+        assertEquals(centArray[24], 97);
+        
+    }
+}
